344a17bdf78b55f97d9ad16b9fa9d26e340127a9
Sana Najjah <sananajjah4@gmail.com>
2025-05-15 12:52:15 +0200

    modifications

--- src/Pages/Consultation.js
+++ src/Pages/Consultation.js
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
-=======
-import React from 'react';
->>>>>>> 6180aa15309943c8f93d3b7a776b88cf56582ef9
 import './Consultation.css';
 import homme from '../assets/homme.svg'; 
 import { FaFacebookF, FaXTwitter, FaInstagram } from 'react-icons/fa6';
 
 const Consultation = () => {
-<<<<<<< HEAD
   const [formData, setFormData] = useState({
     service: '',
     nom: '',
@@ -135,100 +130,3 @@ setSuccessMessage("Merci ! Votre demande a bien été envoyée. Un conseiller vo
 };
 
 export default Consultation;
-=======
-    return (
-        <div className="consultation-container">
-            <div className="consultation-columns">
-                <div className="consultation-text">
-                <h1>Vous avez un projet ? <br/>Contactez-nous pour  <br/>planifier votre <br/> première  <br/>consultation <br/> gratuite !</h1>
-                <p>
-                    Nous sommes heureux de vous offrir une consultation <br/> initiale gratuite. 
-                    C’est l’occasion idéale pour discuter de vos <br/> besoins en entreprise et découvrir 
-                    comment nos services <br/> peuvent vous aider à atteindre vos objectifs.Que vous<br/>  cherchiez des conseils, une formation sur mesure ou une <br/> stratégie d’affaires, notre première rencontre est un pas<br/> vers votre réussite.
-                </p>
-                </div>
-
-                <form className="consultation-form">
-                <label>Service souhaité :</label>
-
-                <select>
-                
-                    <option>Comptabilité</option>
-                    <option>Conseil en gestion</option>
-                    <option>Conseil fiscal</option>
-                    <option>Création d’entreprise</option>         
-                    <option>Conseil Financier</option>
-                    <option>Conseil Juridique</option>
-                    <option>RH et Conseil Social</option>
-                    <option>Formations</option>
-                    <option>Accompagnement sur mesure</option>
-
-                </select>
-
-                <label>Nom :</label>
-                <input type="text" placeholder="Nom" required />
-
-                <label>Email :</label>
-                <input type="email" placeholder="Adresse e-mail" required />
-
-                <label>Téléphone :</label>
-                <input type="tel" placeholder="Numéro de téléphone" required />
-
-                <label>Date et heure préférées :</label>
-                <input type="datetime-local" />
-
-                <label>Questions spécifiques :</label>
-                <textarea rows="4" placeholder="Vos questions..."></textarea>
-
-                <div className="checkbox-container">
-                    <input className='box'type="checkbox" id="acceptData" name="acceptData" />
-                    <label htmlFor="acceptData">J'accepte que mes données soient collectées.</label>
-                </div>
-
-                <button type="submit">Envoyer ma demande</button>
-                </form>
-            </div>
-
-          
-            <div className="consultation-image">
-                <img src={homme} alt="Homme vecteur" />
-            </div>
-
-           
-            <div className="consultation-contact">
-                <h1>Pour toutes questions avant votre consultation gratuite, ou si vous avez besoin d’aide pour la réservation, n’hésitez pas à nous contacter</h1>
-                <div data-number="01">
-                    <span>01 53 10 32 06</span>
-                </div>
-                <div data-number="02">
-                    <span> 60 Rue de La Jonquière, 75017 Paris</span>
-                </div>
-                <div data-number="03">
-                    <span>contact@kof-experts.ma</span>
-                </div>
-            </div>
-            
-            
-
-
-
-        </div>
-        
-        
-        
-            
-            
-
-    );
-};
-export default Consultation;
-
-
-
-
-
-
-            
-
-            
->>>>>>> 6180aa15309943c8f93d3b7a776b88cf56582ef9
